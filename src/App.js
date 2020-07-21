eac714afdd1d98d209975549f9008a8c81b86d66
Vachirarat Thavisaengsiri <pae9769@gmail.com>
2020-07-21 20:08:01 +0700

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import Login from 'Login';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Test</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Login}></Route>
+      </Switch>
+    </Router>
   );
 }
 
